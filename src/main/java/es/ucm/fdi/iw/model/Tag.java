322ad3a93e949274813a298c0a608d77772dfe43
antoniofm99 <antofe10@ucm.es>
2022-03-01 13:48:50 +0100

    tag

--- src/main/java/es/ucm/fdi/iw/model/Tag.java
+++ src/main/java/es/ucm/fdi/iw/model/Tag.java
@@ -1,12 +1,36 @@
 package es.ucm.fdi.iw.model;
 
 import java.util.List;
-
+import javax.persistence.*;
+@Entity
 public class Tag {
 
-private String name;
-private String description;
-private List<Mentoring> mentoringList;
+    private long id;
+    private String name;
+    private String description;
+    private List<Mentoring> mentoringList;
 
-
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    public long getId(){
+        return id;
+    }
+    public String getDescription(){
+        return description;
+    }
+    public List<mentoring> getMentorList(){
+        return mentoringList;
+    }
+    public String getName(){
+        return name;
+    }
+    public void setId(long id){
+        this.id = id;
+    }
+    public void setDescription(String description){
+        this.description = description;
+    }
+    public void setmentoringList(List<Mentoring> mentoringList){
+        this.mentoringList =mentoringList;
+    }
 }
