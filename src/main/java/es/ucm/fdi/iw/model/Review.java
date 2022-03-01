d39ed8613fc52ef1964ce56683a7f8ea855f319b
anonymous <anonymous@anonymous>
2022-03-01 13:25:13 +0100

    Update Review.java

--- src/main/java/es/ucm/fdi/iw/model/Review.java
+++ src/main/java/es/ucm/fdi/iw/model/Review.java
@@ -1,20 +1,18 @@
 package es.ucm.fdi.iw.model;
+import javax.persistence.*;
 import lombok.Data;
 
 @Entity
 @Data
 public class Review {
 
+@Id
+@GeneratedValue(strategy = GenerationType.SEQUENCE, generator = "gen")
+@SequenceGenerator(name = "gen", sequenceName = "gen")
 private long id;
 private int score;
 private Profile profile;
 private String text;
 private Mentoring mentoring;
-
-@Id
-@GeneratedValueue(strategy = GenerationType.IDENTITY)
-public long getId() {
-return id;
-}
 
 }
