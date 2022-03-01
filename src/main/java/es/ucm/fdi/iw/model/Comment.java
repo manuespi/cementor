c85b2414cca265466f64ff01db05a9105d05b336
antoniofm99 <antofe10@ucm.es>
2022-03-01 14:06:27 +0100

    tag comment

--- src/main/java/es/ucm/fdi/iw/model/Comment.java
+++ src/main/java/es/ucm/fdi/iw/model/Comment.java
@@ -1,9 +1,22 @@
 package es.ucm.fdi.iw.model;
+import javax.persistence.*;
 
+import lombok.Data;
+@Entity
+@Data
 public class Comment {
 
-private Tag tag;
-private String text;
-private String username;
+    private long id;
+    @OneToMany
+    private Tag tag;
+    private String text;
+    @ManyToOne
+    private User user;
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    public long getId(){
+        return id;
+    }
 
 }
--- src/main/java/es/ucm/fdi/iw/model/Tag.java
+++ src/main/java/es/ucm/fdi/iw/model/Tag.java
@@ -1,36 +1,22 @@
 package es.ucm.fdi.iw.model;
 
-import java.util.List;
+
 import javax.persistence.*;
+
+import lombok.Data;
 @Entity
+@Data
 public class Tag {
 
     private long id;
     private String name;
     private String description;
-    private List<Mentoring> mentoringList;
+    @ManyToMany
+    private Mentoring mentoring;
 
     @Id
     @GeneratedValue(strategy = GenerationType.IDENTITY)
     public long getId(){
         return id;
-    }
-    public String getDescription(){
-        return description;
-    }
-    public List<Mentoring> getMentorList(){
-        return mentoringList;
-    }
-    public String getName(){
-        return name;
-    }
-    public void setId(long id){
-        this.id = id;
-    }
-    public void setDescription(String description){
-        this.description = description;
-    }
-    public void setmentoringList(List<Mentoring> mentoringList){
-        this.mentoringList =mentoringList;
     }
 }
